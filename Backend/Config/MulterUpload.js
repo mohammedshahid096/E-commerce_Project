96cee9dcf1b1dfcc037f2c126550f30e1e6ca16a
Mohammed shahid <84904302+mohammedshahid096@users.noreply.github.com>
2023-05-30 16:11:32 +0530

    deleting the files

--- Backend/Config/MulterUpload.js
+++
@@ -1,9 +0,0 @@
-import multer from "multer";
-
-const storage = multer.diskStorage({});
-
-const singlUpload = multer({ storage }).single("ProfileAvatar");
-
-export const multipleUpload = multer({ storage }).array("productImages");
-
-export default singlUpload;
